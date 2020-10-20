22295d2295f89fc032b33d3e4fdb7fba66987f8a
Compsognathus07DHS <70004045+Compsognathus07DHS@users.noreply.github.com>
2020-10-20 14:02:27 +0800

    Create myapp.py
    also added bot func

---
+++ myapp.py
@@ -0,0 +1,5 @@
+print("Title of programme: Compsognathus bot")
+print()
+while True:
+  description = input("HI! I am a compsognathus bot! I'll be here to chat whenever you feel like you have something to share, but, well, a dinosaur might handle it better than another human."
+  
